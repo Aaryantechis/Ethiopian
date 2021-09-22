4ce3fa6591648ce6ae97d1b23c7df6d13bd232c5
aaryantechis <aaryan@techis.io>
2021-09-22 23:23:12 +0530

    final push

--- frontend/src/components/Posts/Common/Card.jsx
+++ frontend/src/components/Posts/Common/Card.jsx
@@ -1,15 +1,41 @@
-import React from "react";
-
+import Imglike from "../../../assets/img/likebutton.svg";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavourite } from "../../../reducks/favourites/operations";
+import { getFavourites } from "../../../reducks/favourites/selectors";
 const Card = ({ place }) => {
+  const dispatch = useDispatch();
+  const clickFavourite = (place) => {
+    dispatch(addFavourite(place));
+  };
+  const selector = useSelector((state) => state);
+  const favourites = getFavourites(selector);
+  const [showLikeButton, setShowLikeButton] = useState(true);
+  useEffect(() => {
+    let favoritePlace = favourites.filter(
+      (favourite) => favourite.id == place.id
+    );
+    if (favoritePlace.length > 0) {
+      setShowLikeButton(false);
+    }
+  }, [favourites]);
   return (
     <>
       <div class="card" key={place.id}>
         <div class="row">
           <div class="thumb">
             <img class="thumbnail" src={place.image} alt="" />
-            <div class="like">
-              <img src="images/page1like.svg" alt="" />
-            </div>
+            {showLikeButton && (
+              <div class="like">
+                <img
+                  src={Imglike}
+                  onClick={() => {
+                    clickFavourite(place);
+                  }}
+                  alt=""
+                />
+              </div>
+            )}
           </div>
           <div class="textarea">
             <p class="thumbheading">{place.name}</p>
