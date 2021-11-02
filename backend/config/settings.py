fbe6ff5b2dff955fd1b1c761d29b3fdcd271e492
aaryantechis <aaryan@techis.io>
2021-11-02 14:52:58 +0530

    added database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dch7n72frg031v',
-        'USER': 'ldpelxievviuhf',
+        'NAME': 'd62p88djiflh8r',
+        'USER': 'fydfukbplcsdwn',
         'PORT': 5432,
-        'HOST': 'ec2-23-23-164-251.compute-1.amazonaws.com',
-        'PASSWORD': '8a9353d3c2aa7a6baaf128ed3c6fc0a4ede1cccfec1ccc0222e089e0871b8975',
+        'HOST': 'ec2-44-197-40-76.compute-1.amazonaws.com',
+        'PASSWORD': 'c289eb1ae3c1910512318a282ca293c7a92decdf2a8d22551b36fc8084b51c0d',
     }
 }
 
